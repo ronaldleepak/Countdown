e2f89f1a8c5dd8c7fb77e8221e4ba055af9cd915
Ronald Lee <ronaldleepakho@gmail.com>
2023-02-26 17:06:16 +0100

    Create xcode project

---
+++ Countdown/Countdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countdown
+//
+//  Created by Ronald Lee on 26/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Countdown/Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Ronald Lee on 26/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
